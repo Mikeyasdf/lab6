a872c78cfba68c9de2060134c794bc470bf97d21
EC2 Default User <ec2-user@ip-172-31-25-215.us-east-2.compute.internal>
2020-03-16 04:07:20 +0000

    Solar system intial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+app.engine('html', require('ejs').renderFile);
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+   res.render("index.html");
+});
+
+app.get("/mercury", function(req, res){
+   res.render("mercury.html");
+});
+
+app.get("/venus", function(req, res){
+   res.render("venus.html"); 
+});
+
+app.get("/earth", function(req, res){
+   res.render("earth.html");
+});
+
+app.get("*", function(req, res){
+   res.send("Error Page Not Found");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Running Express Server...");
+});
